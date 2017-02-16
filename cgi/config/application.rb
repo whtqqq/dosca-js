5d82370b36a4d5168b00f25b2ec6fe2de27800a2
Lambert Zhang <tyouteis@gmail.com>
2017-02-17 01:25:07 +0900

    Fix the bug:action name anaylize error

--- cgi/config/application.rb
+++ cgi/config/application.rb
@@ -1,14 +1,13 @@
 class Application
-  ROOT_CONTROLLER = :login
-  DEFAULT_ACTION = :index
+  ROOT_CONTROLLER = "login"
+  DEFAULT_ACTION = "index"
 
   class << self
     def get_controller_and_action_name
       url_split = ENV["REQUEST_URI"].split("/").delete_if{|element| element.empty?}
       controller = url_split[1] || ROOT_CONTROLLER
       action = url_split[2] || DEFAULT_ACTION
-      $stderr.puts action
-      action = action.split("?")[0] unless action.index("?").nil?
+      action = action.split("?")[0] unless  action.index("?").nil?
       {:controller => controller, :action => action}
     end
 
